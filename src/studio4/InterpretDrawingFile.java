9353423866c6a1500b4abf3477d88fa8fdec3cc9
lauren <lauren@DESKTOP-I33DL7A.dhcp.wustl.edu>
2024-10-02 15:05:33 -0500

    pusshshshshshshsh

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,43 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double xOne = in.nextInt();
+		double yOne = in.nextInt();
+		double xTwo = in.nextInt();
+		double yTwo = in.nextInt();
+				
+		StdDraw.setPenColor(red, green, blue);
+		
+		if (shape.equals("triangle")) {
+			if (filled) {
+				double [] x = {xOne, yTwo, xTwo};
+				double [] y = {xOne, yTwo, xTwo};
+				StdDraw.filledPolygon(x, y);
+			} else {
+				double [] x = {xOne, yTwo, xTwo};
+				double [] y = {xOne, yTwo, xTwo};
+				StdDraw.polygon(x, y);
+			}	
+		} else if (shape.equals("rectangle")) {
+			if (filled) {
+				StdDraw.filledRectangle(xOne, yOne, xTwo, yTwo);
+			} else {
+				StdDraw.rectangle(xOne, yOne, xTwo, yTwo);
+			}
+		} else if (shape.equals("ellipse")) { //use this way to show strings are equivalent
+			if (filled) {
+				StdDraw.filledEllipse(xOne, yOne, xTwo, yTwo);
+			} else {
+				StdDraw.ellipse(xOne, yOne, xTwo, yTwo); //automatically unfilled b/c not specified
+			}
+		}
+		
+		
+		
 	}
 }

78ed585067624a10d95276bf7d29b340d2b52262
lizbethguijarro <lizbethguijarro@localhost>
2024-10-02 15:28:38 -0500

    to LLLLLLLLAuren

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,19 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
-	}
-}
+			StdDraw.clear();
+			StdDraw.setPenColor(248,144,0);
+			StdDraw.filledRectangle(0.5 ,0.75,0.5,0.25);
+			StdDraw.setPenColor(248,144,172);
+			StdDraw.filledRectangle(0.5 ,0.25,0.5,0.25);
+			StdDraw.setPenColor(75,175,194);
+			StdDraw.filledCircle (0.5 ,0.5,0.2);
+			
+	}}
+			
+			
+			
+			
+			
+
+	
